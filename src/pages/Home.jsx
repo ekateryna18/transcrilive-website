af0adf623ebc93edeb001def345720e1e43b1e50
Munteanu Ecaterina <munteanu_ecaterina@yahoo.com>
2025-10-24 12:06:01 +0200

    init project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+function Home() {
+  return (
+    <div className="bg-blue-500/100 min-h-screen">
+      <h1 className="text-4xl mb-4">TranscriLive</h1>
+      <p className="text-lg max-w-xl text-center">Making voice accessible to everyone</p>
+      <p>Revolutionize your conferences and meetings with our advanced Live transcription technology.</p>
+    </div>
+  );
+}
+
+export default Home;
